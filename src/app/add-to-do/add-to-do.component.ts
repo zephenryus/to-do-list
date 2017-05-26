2f1a9e24145663f37d12345589aa55d09ea978d4
Kevin Jensen <kevin@turtlerockweb.com>
2017-05-26 16:03:05 -0600

    added documentation and copyright

--- src/app/add-to-do/add-to-do.component.ts
+++ src/app/add-to-do/add-to-do.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
+/**
+ * Form for adding new tasks to the to-do list. app.component is notified by newTaskAdded event.
+ */
 @Component({
     selector: 'app-add-to-do',
     templateUrl: './add-to-do.component.html',
     styleUrls: ['./add-to-do.component.css']
 })
-export class AddToDoComponent implements OnInit {
+export class AddToDoComponent {
     newTask: string;
     @Output() newTaskAdded = new EventEmitter<string>();
 
-    constructor () {
-    }
-
-    ngOnInit () {
-    }
-
+    /**
+     * Emits newTaskAdded event with the new tasks name/label
+     * Clears the newTask input
+     */
     addTask () {
         if (this.newTask) {
             this.newTaskAdded.emit(this.newTask);
